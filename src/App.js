8eafd6ce2819162d3508410916f29272833dd429
Tony Kieling <tony.kieling@gmail.com>
2019-05-31 09:01:54 -0700

    setting and starting react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
 import ReactModal from 'react-modal'
+import { Button, Form } from 'react-bootstrap'
 
 const customStyles = {
   content : {
-    width: "100%",
-    height: "100%",
-    left: "0",
-    top: "0"
-    // top                   : '50%',
-    // left                  : '50%',
-    // right                 : 'auto',
-    // bottom                : 'auto',
-    // marginRight           : '-50%',
-    // transform             : 'translate(-50%, -50%)'
+    // width: "100%",
+    // height: "100%",
+    // left: "0",
+    // top: "0"
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
   }
 };
 
@@ -39,6 +40,7 @@ class App extends React.Component {
 
   customStyles = {
     content: {
+      left: eval("50%" - 100),
       width: 200
     }
   }
@@ -55,15 +57,42 @@ class App extends React.Component {
           // closeTimeoutMS={500}
           shouldCloseOnEsc={true}
           contentLabel="Minimal Modal Example"
-          style={this.customStyles}
+          style={customStyles}
         >
           <h1>Modal title</h1>
           <p>modal p example</p>
-          <p>modal p example</p>
-          <p>modal p example</p>
-          <p>modal p example</p>
-          <p>modal p example</p>
-          <button onClick={this.handleCloseModal}>Close Modal</button>
+          <Form onSubmit={this.handleSubmit}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>User / Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Type the user's email"
+                        name="email"
+                        onChange={this.handleChange}
+                        value={this.state.email}
+                    />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+
+            </Form>
+            {/* <button onClick={this.handleCloseModal}>Close Modal</button> */}
         </ReactModal>
       </div>
     );
