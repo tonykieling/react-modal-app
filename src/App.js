56f592d128c52d666cbd060d5a2a4989a1c750ee
Tony Kieling <tony.kieling@gmail.com>
2019-05-31 12:01:52 -0700

    css is working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './App.css';
-// import ReactModal from 'react-modal'
+import ReactModal from 'react-modal'
 import {Button, Form} from 'react-bootstrap'
 
-// const customStyles = {
-//   content : {
-//     // width: "100%",
-//     // height: "100%",
-//     // left: "0",
-//     // top: "0"
-//     top                   : '50%',
-//     left                  : '50%',
-//     right                 : 'auto',
-//     bottom                : 'auto',
-//     marginRight           : '-50%',
-//     transform             : 'translate(-50%, -50%)'
-//   }
-// };
+const customStyles = {
+  content : {
+    // width: "100%",
+    // height: "100%",
+    // left: "0",
+    // top: "0"
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -52,12 +52,21 @@ class App extends React.Component {
   render() {
     return (
       <div className="moldura">
-      <h1>Login Page</h1>    
-      <div className="d-flex justify-content-end">
-        <Button onClick={this.handleSubmit} variant="outline-success">
-          Close me ya'll!
+      <h1>Login Page</h1>
+      <Button onClick={this.handleOpenModal}>
+        Open Modal
+      </Button>
+
+      <ReactModal
+        isOpen = {this.state.showModal}
+        style = {customStyles}
+      >
+        <h1>Modal</h1>
+        <Button onClick={this.handleCloseModal}>
+          Close Modal
         </Button>
-      </div>
+      </ReactModal>
+      
           {/* <Form onSubmit={this.handleSubmit}>
               <Form.Group controlId="formBasicEmail">
                   <Form.Label>User / Email address</Form.Label>
@@ -89,7 +98,6 @@ class App extends React.Component {
               </Button>
 
           </Form> */}
-
     </div>
 
     );
