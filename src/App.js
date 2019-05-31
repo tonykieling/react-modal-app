73ee24359327d51b77eb7f6929dc578912965f44
Tony Kieling <tony.kieling@gmail.com>
2019-05-31 11:36:22 -0700

    settings

--- src/App.js
+++ src/App.js
@@ -1,83 +1,99 @@
-import React, { Component } from 'react'
+import React from 'react';
+import './App.css';
+// import ReactModal from 'react-modal'
 import {Button, Form} from 'react-bootstrap'
 
+// const customStyles = {
+//   content : {
+//     // width: "100%",
+//     // height: "100%",
+//     // left: "0",
+//     // top: "0"
+//     top                   : '50%',
+//     left                  : '50%',
+//     right                 : 'auto',
+//     bottom                : 'auto',
+//     marginRight           : '-50%',
+//     transform             : 'translate(-50%, -50%)'
+//   }
+// };
 
-export default class App extends Component {
-  constructor(props){
-    super(props);
+class App extends React.Component {
+  constructor(props) {
+    super(props)
     this.state = {
-      email: "",
-      password: ""
+      showModal: false
     }
   }
 
-  handleChange = e => {
+  handleOpenModal = () => {
     this.setState({
-      [e.target.name]: e.target.value
-    });
+      showModal: true
+    })
   }
 
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    // const user = checkUser({email: this.state.email, password: this.state.password})
-    const user = true
-    if (!user) {
-      alert("Email/Password is wrong!");
-      this.setState({
-        email: "",
-        password: ""
-      })
-      return;
-    } else {
-      // store.dispatch({type: "LOGIN", data: { user }})
-      console.log("login is valid!")
+  handleSubmit = () => {
+    console.log("handlesubmit")
+  }
+
+  customStyles = {
+    content: {
+      left: eval("50%" - 100),
+      width: 200
     }
-  }
-
-  isValid = () => {
-    if (this.state.email === "" || this.state.password === "")
-      return false;
-    return true;
   }
 
   render() {
     return (
       <div className="moldura">
-        <h1>Login Page</h1>      
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>User / Email address</Form.Label>
-                    <Form.Control
-                        type="email"
-                        placeholder="Type the user's email"
-                        name="email"
-                        onChange={this.handleChange}
-                        value={this.state.email}
-                    />
-                    <Form.Text className="text-muted">
-                      We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                    />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>
-
-            </Form>
-
+      <h1>Login Page</h1>    
+      <div className="d-flex justify-content-end">
+        <Button onClick={this.handleSubmit} variant="outline-success">
+          Close me ya'll!
+        </Button>
       </div>
-    )
+          {/* <Form onSubmit={this.handleSubmit}>
+              <Form.Group controlId="formBasicEmail">
+                  <Form.Label>User / Email address</Form.Label>
+                  <Form.Control
+                      type="email"
+                      placeholder="Type the user's email"
+                      name="email"
+                      onChange={this.handleChange}
+                      value={this.state.email}
+                  />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                  />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+
+          </Form> */}
+
+    </div>
+
+    );
   }
 }
+
+export default App;
